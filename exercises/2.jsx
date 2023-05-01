53493ea582226166292a6a3c28dfee3dcf7bcb23
MaiCodes-exe <maise.b.costa@gmail.com>
2023-05-01 19:44:57 +0000

    2nd done

---
+++ exercises/2.jsx
@@ -0,0 +1,9 @@
+// Get State from the Redux Store
+
+
+// const store = Redux.createStore(
+//   (state = 5) => state
+// );
+
+// // Change code below this line
+// const currentState = store.getState();
