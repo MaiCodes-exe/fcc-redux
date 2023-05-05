fde7693955e0bae25067d8cdd430f3d1b198c7b2
MaiCodes-exe <maise.b.costa@gmail.com>
2023-05-05 20:12:24 +0000

    5th done

--- exercises/5.jsx
+++ exercises/5.jsx
@@ -1,1 +1,13 @@
-// Dispatch an Action Event
+// // Dispatch an Action Event
+// const store = Redux.createStore(
+//   (state = {login: false}) => state
+// );
+
+// const loginAction = () => {
+//   return {
+//     type: 'LOGIN'
+//   }
+// };
+
+// // Dispatch the action here:
+// store.dispatch(loginAction());
